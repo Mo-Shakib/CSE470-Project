11d3d0c899efa30e86913565ee104b230488fbc3
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2023-11-04 04:52:40 +0600

    updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import { TbCheckbox } from 'react-icons/tb'
+
+
 export const Header = () => {
-  return (
+    const [checked, setChecked] = React.useState(false);
+    
+    return (
+    
     <header className="App-header">
-      <p>
-        Hello, There! <br />
-        <small>from Header.js</small>
-      </p>
+      <nav>
+        <div className='logo'>
+            <h1>Logo</h1>
+        </div>
+        <div className='settings'>
+            <ul>
+                <li><TbCheckbox/> Hello</li>
+                <li><TbCheckbox /></li>
+                <li><TbCheckbox /></li>
+                <li><TbCheckbox /></li>
+            </ul>
+        </div>
+
+        </nav>
     </header>
   );
 }
