2febe7ae00ee6007dd5a8ec36dff150162819b5e
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2023-11-03 23:36:14 +0600

    Header file

---
+++ src/components/layout/Header.js
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header className="App-header">
+      <p>
+        Hello, There! <br />
+        <small>from Header.js</small>
+      </p>
+    </header>
+  );
+}
